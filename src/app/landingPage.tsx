8df6199a890d374ce129cfa4f913bc8d74b73c34
laurynas <unextbusinessman@gmail.com>
2024-05-17 00:36:40 +0200

    landing page

---
+++ src/app/landingPage.tsx
@@ -0,0 +1,16 @@
+import HeroComponent from "@/components/layout/Hero";
+
+export default function Home() {
+  return (
+    <div>
+      <HeroComponent />
+      <section className="pb-20 lg:mt-12 px-6">
+        <img
+          src="/bg.png"
+          alt="bg of project"
+          className="mx-auto w-full lg:max-w-5xl rounded-xl shadow-xl"
+        />
+      </section>
+    </div>
+  );
+}
