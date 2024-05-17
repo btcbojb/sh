f1e443de9c574a5058f280f7f41e27dc23994778
Alexander Esbjörnsson <alex.esbj@gmail.com>
2024-05-17 10:48:55 +0200

    rounded icons, better colors, layout

--- src/app/map/[place]/page.tsx
+++ src/app/map/[place]/page.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { neon } from "@neondatabase/serverless";
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/neon-http";
 import {
   Accessibility,
   Dog,
@@ -7,15 +11,17 @@ import {
   Nut,
   Speech,
   Volume2,
+  Grip,
+  CigaretteOff,
+  ThumbsUp
 } from "lucide-react";
 import React from "react";
 
-const tantGron = {
+const data = {
   id: "1",
   name: "Tant Grön",
   availability: {
     Wheelchair: "rgba(0, 128, 0, 0.7)", // green
-    Perfume: "rgba(255, 0, 0, 0.7)", // red
     Volume: "rgba(255, 0, 0, 0.7)", // red
     Languages: "rgba(255, 165, 0, 0.7)", // orange
     ElevationDiffrence: "rgba(128, 128, 128, 0.7)", // gray
@@ -31,25 +37,27 @@ const tantGron = {
 function renderIcon(key: string): JSX.Element {
   switch (key) {
     case "Wheelchair":
-      return <Accessibility size={64} color="green" />;
+      return <Accessibility size={64} />;
     case "Perfume":
-      return <Speech size={64} color="green" />;
+      return <Speech size={64} />;
     case "Volume":
-      return <Volume2 size={64} color="green" />;
+      return <Volume2 size={64} />;
     case "Languages":
-      return <Languages size={64} color="orange" />;
+      return <Languages size={64} />;
     case "ElevationDiffrence":
-      return <Layers2 size={64} color="red" />;
+      return <Layers2 size={64} />;
     case "Nuts":
-      return <Nut size={64} color="red" />;
+      return <Nut size={64} />;
     case "AuditoryLoop":
-      return <Ear size={64} color="gray" />;
+      return <Ear size={64} />;
     case "SignLanguage":
-      return <Languages size={64} color="gray" />;
+      return <Languages size={64} />;
     case "Smoke":
-      return <Nut size={64} color="gray" />;
+      return <CigaretteOff size={64} />;
     case "DogFriendly":
-      return <Dog size={64} color="gray" />;
+      return <Dog size={64} />;
+    case "Carpet":
+      return <Grip size={64} />
     default:
       return <></>;
   }
@@ -58,9 +66,7 @@ function renderIcon(key: string): JSX.Element {
 function translateKey(key: string): string {
   switch (key) {
     case "Wheelchair":
-      return "Rullstolstillgänglig";
-    case "Perfume":
-      return "Doftfri miljö";
+      return "Rörelsehinder";
     case "Volume":
       return "Ljudnivå";
     case "Languages":
@@ -77,61 +83,71 @@ function translateKey(key: string): string {
       return "Rökfri miljö";
     case "DogFriendly":
       return "Hundvänlig";
+    case "Carpet":
+      return "Heltäcknigsmatta"
     default:
       return key;
   }
 }
 
-export default function PlacePage() {
+export default async function PlacePage() {
+
+  // const sql = neon(
+  //   "postgresql://includer_owner:c1bK9uLFCxRt@ep-tight-rice-a2lfuh7m.eu-central-1.aws.neon.tech/includer?sslmode=require"
+  // );
+  // const db = drizzle(sql);
+  // const placeData = await db
+  //   .select()
+  //   .from(placeTable)
+  //   .where(eq(placeTable.placeId, "HERE YOU WRITE PARAMS"));
+
   return (
     <div>
-      <div className="mt-24 flex flex-col space-y-4">
+      <div className="flex flex-col space-y-4">
         <div
           className="w-full h-32 sm:h-48 md:h-56 lg:h-68 sm flex items-center pl-4"
           style={{
             backgroundImage: "url('/images/tantgron.png')",
             backgroundSize: "cover",
-            filter: "grayscale(65%)",
           }}
         >
-          <h2
-            className="text-3xl lg:text-4xl font-semibold text-white uppercase"
-            style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}
-          >
-            {tantGron.name}
-          </h2>
         </div>
-        <div className="container mx-auto flex flex-col lg:flex-row gap-8">
-          {/* Disability */}
-          <div className="lg:w-3/4">
-            <h3 className="text-2xl">Tillgänglighet</h3>
-            <div className="flex flex-col md:flex-row gap-4 mt-2">
-              <span className="bg-[#f6f6f6] p-1 px-2 rounded-md">
+        <div className="container mx-auto flex flex-col gap-8">
+          {/* Availability */}
+          <div>
+            <h2 className="text-3xl py-6 text-center">{data.name}</h2>
+            <div className="flex flex-col justify-center md:flex-row gap-6 mt-6">
+              <span className="bg-[#f6f6f6] p-1 px-2 rounded-full">
                 🟢 = God tillgänglighet
               </span>
-              <span className="bg-[#f6f6f6] p-1 px-2 rounded-md">
+              <span className="bg-[#f6f6f6] p-1 px-2 rounded-full">
                 🟠 = Mellan tillgänglighet
               </span>
-              <span className="bg-[#f6f6f6] p-1 px-2 rounded-md">
+              <span className="bg-[#f6f6f6] p-1 px-2 rounded-full">
                 🔴 = Dålig tillgänglighet
               </span>
-              <span className="bg-[#f6f6f6] p-1 px-2 rounded-md">
+              <span className="bg-[#f6f6f6] p-1 px-2 rounded-full">
                 ⚫ = Ingen information
               </span>
             </div>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 mt-4 rounded-md bg-[#dadada] p-2">
-              {Object.entries(tantGron.availability).map(
+            <div className="flex flex-wrap justify-center gap-12 mt-6 m-0 bg-slate-400 py-12">
+              {Object.entries(data.availability).map(
                 ([key, value], index) => (
-                  <div
-                    key={index}
-                    className={`flex items-center space-x-2 border-2 flex-col rounded-md p-4`}
-                    style={{
-                      backgroundColor: `${value}`,
-                    }}
-                  >
-                    <p className="text-lg font-medium">{translateKey(key)}</p>
-                    {renderIcon(key)}
-                    <p>{value === "green" ? ":)" : ":("}</p>
+                  <div className="flex flex-col">
+                    <div
+                      key={index}
+                      className={`flex items-center space-x-2  flex-col rounded-full p-6 w-40 h-40`}
+                      style={{
+                        backgroundColor: `${value}`,
+                      }}
+                    >
+                      {renderIcon(key)}
+                      <p className="text-lg">{translateKey(key)}</p>
+                    </div>
+                    <span className="flex w-min mt-6 gap-4 m-auto justify-center">
+                      <Button>👍</Button>
+                      <Button>👎</Button>
+                    </span>
                   </div>
                 )
               )}
