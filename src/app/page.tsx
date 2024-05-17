933d4a66c202c9a144be05e7613988f27e61a68b
ljb-developer <ljb-developer@webinno.se>
2024-05-17 12:35:36 +0200

    Late fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,7 @@ export default function Home() {
     <div>
       <HeroComponent />
       <section className="pb-20 lg:mt-14 px-6">
-        <img
-          src="/bg.png"
-          alt="bg of project"
-          className="mx-auto w-full lg:max-w-5xl rounded-xl shadow-xl"
-        />
+      
       </section>
     </div>
   );
